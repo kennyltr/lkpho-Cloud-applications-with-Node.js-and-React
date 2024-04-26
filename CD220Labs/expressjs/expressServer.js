6f1217f2e01531070a53adc39a83b210209bfc36
kenny <kennyltr@hotmail.com>
2024-04-25 22:08:31 -0400

    added month

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = new express();
 
 let loginDetails = [];
+let monthlist = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 app.get("/",(req,res)=>{
     res.send("Welcome to the express server")
@@ -18,6 +19,19 @@ app.post("/login/:name",(req,res)=>{
 
 app.get("/:name",(req,res)=>{
     res.send("Hello "+req.params.name)
+})
+
+app.get("/fetchMonth/:num",(req,res)=>{
+    let nummonth = parseInt(req.params.num);
+    if (nummonth > 12 || nummonth < 1) {
+        res.send("Invalid month number!")
+    }
+    else if (isNaN(nummonth)) {
+        res.send("Please input a number for the month!")
+    }
+    else {
+        res.send("The month is "+ monthlist[(nummonth-1)])
+    }
 })
 
 app.listen(3333, () => {
